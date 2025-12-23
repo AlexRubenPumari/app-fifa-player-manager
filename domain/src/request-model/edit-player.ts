8eb58d4ce2f6361fb40b750166db399c2d39a820
alex <alexpumari0@gmail.com>
2025-12-23 19:52:45 -0300

    feat(domain): add edit-player request model

---
+++ domain/src/request-model/edit-player.ts
@@ -0,0 +1,20 @@
+import * as z from "zod"
+import type { Player } from "../entities"
+import type { Updatable, Validator } from "../helpers"
+
+export interface EditPlayerPayload {
+  player: Updatable<Player>
+}
+
+const hasUpdatableField = (data: Record<string, unknown>) =>
+  data.fullName || data.nationality
+
+const UpdatablePlayerSchema = z.object({
+  id: z.number().gte(1),
+  fullName: z.string().min(1).optional(),
+  nationality: z.string().min(1).optional(),
+}).refine(hasUpdatableField)
+
+export const validatePayload: Validator<EditPlayerPayload> = ({ player }) => {
+  return UpdatablePlayerSchema.safeParse(player).success
+}
--- domain/src/request-model/index.ts
+++ domain/src/request-model/index.ts
@@ -1,1 +1,2 @@
 export * from "./create-player"
+export * from "./edit-player"
