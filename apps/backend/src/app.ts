d74e7279e7855a341f3b31b76ea8426d699ca6eb
alex <alexpumari0@gmail.com>
2025-12-26 18:52:55 -0300

    feat(backend): add player routes

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,20 +1,13 @@
 import express from "express"
-import { MockPlayerRepository, findPlayer, listPlayers } from "../../../domain/dist/index.js"
-const app = express()
+import { errorHandler } from "./middleware"
+import { playersRouter } from "./routes"
 
+const app = express()
 const PORT: number = 3000
 
-const playerRepository = new MockPlayerRepository([
-  { id: 1, fullName: "Lionel Messi" },
-  { id: 2, fullName: "Lionel Messi" },
-  { id: 3, fullName: "Lionel Messi" },
-  { id: 4, fullName: "Lionel Messi" },
-])
+app.use("/v1/players", playersRouter)
 
-app.get('/v1/players', (_, res) => {
-  const players = listPlayers({ playerRepository }, { options: {} })
-  res.json()
-})
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
---
+++ apps/backend/src/routes/player-routes.ts
@@ -0,0 +1,45 @@
+import { MockPlayerRepository, Player, findPlayer, listPlayers } from "@domain"
+import express, { Response } from "express"
+import { ApiResponse } from "../types"
+
+type FindPlayerApiResponse = ApiResponse<{ player: Player }>
+type ListPlayersApiResponse = ApiResponse<{ players: Player[] }>
+
+const playerRepository = new MockPlayerRepository([
+  { id: 1, fullName: "Lionel Messi" },
+  { id: 2, fullName: "Julian Álvarez" },
+  { id: 3, fullName: "Enzo Fernandez" },
+  { id: 4, fullName: "Lautaro Martinez" },
+])
+
+export const playersRouter = express.Router()
+
+playersRouter.route("/")
+  .get(async (_, res: Response<ListPlayersApiResponse>) => {
+    const result = await listPlayers({ playerRepository }, { options: {} })
+
+    if (result.success) {
+      res.status(200).json({
+        data: { players: result.value.players },
+        message: "Players fetched successfully",
+      })
+    }
+  })
+
+playersRouter.route("/:id")
+  .get(async (req, res: Response<FindPlayerApiResponse>) => {
+    const id: number = Number(req.params.id)
+
+    const result = await findPlayer({ playerRepository }, { options: { id } })
+
+    if (result.success) {
+      res.status(200).json({
+        data: { player: result.value.player },
+        message: "Player fetched successfully",
+      })
+    } else {
+      res.status(404).json({
+        message: result.error,
+      })
+    }
+  })
