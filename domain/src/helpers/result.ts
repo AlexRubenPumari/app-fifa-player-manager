2fc01699720714f47fed2aa7935eb5da80a8c1d8
alex <alexpumari0@gmail.com>
2025-12-26 18:49:02 -0300

    refactor(domain): rewrite result type to enable type discrimination

--- domain/src/helpers/result.ts
+++ domain/src/helpers/result.ts
@@ -1,4 +1,3 @@
-import { AtLeastOne } from "./"
-
 export type Result<ValueType, ErrorType> =
-  { success: boolean } & AtLeastOne<{ value?: ValueType, error?: ErrorType }>
+  | { success: true, value: ValueType }
+  | { success: false, error: ErrorType }
