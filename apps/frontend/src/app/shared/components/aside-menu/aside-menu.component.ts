9b6922d97e8f39fc9dd1fdf50b1f4c9ddc9bfc80
alex <alexpumari0@gmail.com>
2025-12-27 20:39:37 -0300

    feat(frontend): add aside-menu component

---
+++ apps/frontend/src/app/shared/components/aside-menu/aside-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "app-aside-menu",
+  standalone: true,
+  imports: [],
+  templateUrl: "./aside-menu.component.html",
+})
+export class AsideMenu {}
