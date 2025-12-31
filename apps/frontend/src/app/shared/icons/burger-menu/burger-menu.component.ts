2ff4789db03d0eb9745af91e1e292f9d2be6b8ba
alex <alexpumari0@gmail.com>
2025-12-30 21:09:50 -0300

    feat(frontend): add burger-menu icon

---
+++ apps/frontend/src/app/shared/icons/burger-menu/burger-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "icon-burger-menu",
+  standalone: true,
+  imports: [],
+  templateUrl: "./burger-menu.component.html",
+})
+export class BurgerMenu {}
