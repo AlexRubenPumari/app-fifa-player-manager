d7e3b414f087b502719eb38eac9acdead3c28514
alex <alexpumari0@gmail.com>
2026-01-02 18:51:01 -0300

    feat(frontend): implement list-playes view

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,25 +1,15 @@
 import { Component } from "@angular/core"
 import { RouterOutlet } from "@angular/router"
-import { Header, AsideMenu, Title, Table, Footer } from "./shared/components"
+import { Header, AsideMenu, Footer } from "./shared/components"
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, Header, AsideMenu, Title, Footer, Table],
+  imports: [RouterOutlet, Header, AsideMenu, Footer],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css"
 })
 export class App {
-  tableColumns = [
-    { key: "id", label: "Nro" },
-    { key: "name", label: "Nombre" },
-    { key: "pos", label: "Posicion" },
-    { key: "age", label: "Edad" },
-  ]
-  tableData = [
-    { id: 1, name: "Lionel Messi", pos: "MCD", age: 32 },
-    { id: 2, name: "Julian Álvarez", pos: "DEL", age: 24 },
-  ]
   isMenuOpen: boolean = false
 
   toggleMenu() {
--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router"
+import { ListPlayersView } from "./views"
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "players", component: ListPlayersView }
+]
