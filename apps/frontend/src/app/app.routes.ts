f7589d92fc8c263cf113a2b18907188101349865
alex <alexpumari0@gmail.com>
2026-01-02 19:47:04 -0300

    feat(frontnd): add router links in aside-menu component

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from "@angular/router"
 import { ListPlayersView } from "./views"
 
 export const routes: Routes = [
-  { path: "players", component: ListPlayersView }
+  { path: "players", component: ListPlayersView },
+  { path: "skills", component: ListPlayersView },
+  { path: "teams", component: ListPlayersView },
 ]
--- apps/frontend/src/app/shared/components/aside-menu/aside-menu.component.ts
+++ apps/frontend/src/app/shared/components/aside-menu/aside-menu.component.ts
@@ -1,12 +1,24 @@
 import { Component, Input } from "@angular/core"
 import { CommonModule } from "@angular/common"
+import { RouterLink, RouterLinkActive } from "@angular/router"
 
 @Component({
   selector: "app-aside-menu",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: "./aside-menu.component.html",
 })
 export class AsideMenu {
   @Input() isOpen: boolean = false
+
+  asideMenuItems: AsideMenuItem[] = [
+    { label: "Jugadores", route: "/players" },
+    { label: "Habilidades", route: "/skills" },
+    { label: "Equipos", route: "/teams" },
+  ]
+}
+
+interface AsideMenuItem {
+  label: string,
+  route: string,
 }
