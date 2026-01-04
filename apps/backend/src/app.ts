d1c7e89971877df1e580b4c3a6396424b6e9224e
alex <alexpumari0@gmail.com>
2026-01-04 19:42:35 -0300

    feat(backend): connect backend to database

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,14 +1,35 @@
-import express from "express"
-import { errorHandler } from "./middleware"
-import { playersRouter } from "./routes"
+  import express from "express"
+  import { errorHandler } from "./middleware"
+  import { playersRouter } from "./routes"
 
-const app = express()
-const PORT: number = 3000
+  import { Sequelize } from "sequelize"
+  // function iniciarSequelize() {
+  const sequelize = new Sequelize("fifa-manager-database", "root", "root123", {
+    host: "localhost",
+    port: 5000,
+    dialect: "mysql",
+  })
+  // }
 
-app.use("/v1/players", playersRouter)
+  const app = express()
+  const PORT: number = 3000
 
-app.use(errorHandler)
+  app.use("/v1/players", playersRouter)
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  app.use(errorHandler)
+
+  conectarSequelize()
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
+
+
+  async function conectarSequelize() {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+    }
+  }
