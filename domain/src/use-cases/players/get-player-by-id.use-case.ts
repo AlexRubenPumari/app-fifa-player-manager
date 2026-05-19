355715cb66519b4b9f1b8879dd18459c90c23594
alex <alexpumari0@gmail.com>
2026-05-19 16:35:15 -0300

    feat(domain): add get-player-by-id use case

---
+++ domain/src/use-cases/players/get-player-by-id.use-case.ts
@@ -0,0 +1,28 @@
+import { PlayerNotFoundError } from "../../errors";
+import { PlayerRepository } from "../../repositories";
+import { Player } from "../../entities";
+import { createUseCase, createResult, schema } from "../../shared/utils";
+
+interface GetPlayerByIdDependencies {
+  playerRepository: PlayerRepository;
+}
+
+type GetPlayerByIdResponse = Player;
+
+const getPlayerByIdUseCase = createUseCase<
+  GetPlayerByIdDependencies,
+  GetPlayerByIdResponse,
+  PlayerNotFoundError
+>()({
+  isAuthRequired: true,
+  requestShape: {
+    id: schema.number(),
+  },
+  handler: async ({ playerRepository }, request) => {
+    const player = await playerRepository.findOne({ id: request.id });
+
+    if (!player) return createResult.error(new PlayerNotFoundError());
+
+    return createResult.ok(player);
+  },
+});
