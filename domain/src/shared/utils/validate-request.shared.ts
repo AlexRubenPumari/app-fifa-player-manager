d669870b907895bfd94a7947bfa63b0959ff9bf4
alex <alexpumari0@gmail.com>
2026-05-19 10:39:51 -0300

    refactor(domain): rewrite validate-request util

--- domain/src/shared/utils/validate-request.shared.ts
+++ domain/src/shared/utils/validate-request.shared.ts
@@ -1,17 +1,18 @@
-import { Result, Schema, SchemaInput, SchemaOutput } from "../types/core/index";
-import { createResult } from "./create-result.shared";
-import { InvalidRequestError } from "../../errors/index";
+import { InvalidRequestError } from "../../errors";
+import { Request, RequestShape, Result } from "../types";
+import { createResult, schema } from "./";
 
-export function validateRequest<TSchema extends Schema>(
-  schema: TSchema,
-  request: SchemaInput<TSchema>
-): Result<SchemaOutput<TSchema>, InvalidRequestError> {
-  const result = schema.safeParse(request);
-
+export function validateRequest<TShape extends RequestShape>(
+  requestShape: TShape,
+  request: unknown
+): Result<Request<TShape>, InvalidRequestError> {
+  const result = schema.object(requestShape).safeParse(request);
   if (!result.success) {
     const issue = result.error.issues[0];
-    return createResult.error(new InvalidRequestError(issue.message));
+    return createResult.error(
+      new InvalidRequestError(issue.message)
+    );
   }
-
-  return createResult.ok(result.data);
+  
+  return createResult.ok(result.data as Request<TShape>);
 }
