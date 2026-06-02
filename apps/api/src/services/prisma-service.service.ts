936f571a4365d747b43a3bd736679213dd7a4894
alex <alexpumari0@gmail.com>
2026-06-02 19:04:27 -0300

    refactor(api): rewrite and move prisma service module

--- apps/api/src/services/prisma-service.service.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../../generated/prisma/client";
-import { env } from "src/config";
-
-const adapter = new PrismaMariaDb(env.DATABASE_URL)
-
-export const prisma = new PrismaClient({ adapter });
