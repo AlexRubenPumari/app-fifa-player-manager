0939e3bc2a858fd336707e7ae9cac00958954437
alex <alexpumari0@gmail.com>
2026-06-07 23:16:46 -0300

    chore(api): add jwt authentication guard and strategy

---
+++ apps/api/src/services/auth/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { getEnv } from 'src/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: getEnv('ACCESS_TOKEN_SECRET'), // todo: esto corregir env service
+    });
+  }
+
+  validate(payload: any) {
+    return { username: payload.username };
+  }
+}
