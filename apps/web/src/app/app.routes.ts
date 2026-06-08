9a52ba64ec865001ff4b73034485f88d03bcbce1
alex <alexpumari0@gmail.com>
2026-06-07 23:25:42 -0300

    chore(web): update app configuration and routing with new components and guards

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'; //todo: elint " '"
+import { LoginPage, PlayerDetailPage, PlayerEditPage, PlayersListPage } from './pages';
+import { authGuard } from './guards';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "login", component: LoginPage },
+    {
+        path: "players",
+        canActivateChild: [authGuard],
+        children: [
+            { path: "", component: PlayersListPage },
+            { path: ":id/edit", component: PlayerEditPage },
+            { path: ":id", component: PlayerDetailPage },
+        ],
+    },
+    // { path: '', redirectTo: 'login', pathMatch: 'full' }, //todo: q hce?
+    { path: '**', redirectTo: 'login', pathMatch: 'full' }, //todo: q hce?
+];
+ 
